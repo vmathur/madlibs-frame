2ddbb975642d3bee20cec6a317939953b4bc8b01
vmathur <vmathur@users.noreply.github.com>
2024-01-31 14:39:56 -0800

    added random words

--- src/pages/api/post.js
+++ src/pages/api/post.js
@@ -1,7 +1,14 @@
-import { generateFarcasterFrame, generateEndFrame, generateRandomFarcasterFrame } from '@/utils'
+import { generateEndFrame, generateRandomFarcasterFrame } from '@/utils'
 import { kv } from '@vercel/kv';
+import { type } from 'os';
 
 const BASE_URL = process.env.BASE_URL;
+const wordTypeMap = {
+  1 : 'noun',
+  2 : 'verb',
+  3 : 'adjective'
+}
+const storyWordTypeOrder = [1,2,1,2,2,1,1,1,3,1]
 
 export default async function handler (req,res){
   if (req.method !== 'POST') {
@@ -14,35 +21,43 @@ export default async function handler (req,res){
     console.log('theres state')
     // if(state===done){
       return res.status(302).setHeader('Location', `${BASE_URL}`).send('Redirecting');
+      // return res.status(302).setHeader('Location', `https://vmathur.com`).send('Redirecting');
   }
 
   const signedMessage = req.body;
   const choice = signedMessage.untrustedData.buttonIndex
 
+  let round = 0;
   try {
-    const round = await kv.get('round');
-    console.log(round);
+    round = await kv.get('round');
   } catch (error) {
     await kv.set('round', 1);
   }
-
   let html = ''
-  console.log(req.query.screen)
-  if(req.query.screen && req.query.screen==='1'){
-    html = generateRandomFarcasterFrame() 
-  }else if(req.query.screen && req.query.screen==='2'){
-    //todo store the choice;
-    // if (choice === 1) {
-    //   //todo
-    // } else if (choice === 2){
-    //   //todo
-    // } else if (choice ===3 ){
-    //   //todo
-    // } else{
-    //   //todo
-    // }
-    html = generateEndFrame();
-  }
 
+  console.log('round '+round)
+  console.log('choice '+choice)
+  console.log('screen '+req.query.screen)
+  console.log('type '+typeIndex)
+
+  let typeIndex = storyWordTypeOrder[round-1]
+  let type = wordTypeMap[typeIndex]
+
+  if(req.query.screen && req.query.screen==='1'){
+    html = await generateRandomFarcasterFrame(type) 
+  }else if(req.query.screen && req.query.screen==='2'){
+    if (choice === 1) {
+      //todo store req.query.word_1
+      html = generateEndFrame();
+    } else if (choice === 2){
+      //todo store req.query.word_2
+      html = generateEndFrame();
+    } else if (choice ===3 ){
+      //todo store req.query.word_3    
+      html = generateEndFrame();
+    } else{
+      html = await generateRandomFarcasterFrame(type) 
+    }
+  }
   return res.status(200).setHeader('Content-Type', 'text/html').send(html)
 }
--- src/utils.js
+++ src/utils.js
@@ -1,33 +1,13 @@
 export const BASE_URL = process.env.BASE_URL
+const axios = require('axios');
 
-// generate an html page with the relevant opengraph tags
-export function generateFarcasterFrame(image, choice) {
-  return `
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-      <meta property="fc:frame" content="vNext" />
-      <meta property="fc:frame:image" content="${image}" />
-      <meta property="fc:frame:post_url" content="${BASE_URL}/api/post" />
 
-      ${
-        choice === 2
-          ? '<meta property="fc:frame:button:1" content="Yes" />'
-          : ''
-      }
-    </head>
-    <body>
-      
-    </body>
-    </html>
-  `
-}
-
-export function generateRandomFarcasterFrame() {
+export async function generateRandomFarcasterFrame(type) {
   let image = BASE_URL+'game.jpg';
-  const word1 = 'a'
-  const word2 = 'b';
-  const word3 = 'c';
+  
+  let word_1 = await getWord(type)
+  let word_2 = await getWord(type)
+  let word_3 = await getWord(type)
 
   return `
     <!DOCTYPE html>
@@ -35,11 +15,11 @@ export function generateRandomFarcasterFrame() {
     <head>
       <meta property="fc:frame" content="vNext" />
       <meta property="fc:frame:image" content="${image}" />
-      <meta property="fc:frame:button:1" content="${word1}" />
-      <meta property="fc:frame:button:2" content="${word2}" />
-      <meta property="fc:frame:button:3" content="${word3}" />
-      <meta property="fc:frame:button:3" content="🔄 More options" />
-      <meta property="fc:frame:post_url" content="${BASE_URL}/api/post?screen=2" />
+      <meta property="fc:frame:button:1" content="${word_1}" />
+      <meta property="fc:frame:button:2" content="${word_2}" />
+      <meta property="fc:frame:button:3" content="${word_3}" />
+      <meta property="fc:frame:button:4" content="🔄 More options" />
+      <meta property="fc:frame:post_url" content="${BASE_URL}/api/post?screen=2&word_1=${word_1}&word_2=${word_2}&word_3=${word_3}" />
     <body>
     </body>
     </html>
@@ -61,4 +41,46 @@ export function generateEndFrame() {
     </body>
     </html>
   `
+}
+
+export function generateFarcasterFrame(image, choice) {
+  return `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta property="fc:frame" content="vNext" />
+      <meta property="fc:frame:image" content="${image}" />
+      <meta property="fc:frame:post_url" content="${BASE_URL}/api/post" />
+
+      ${
+        choice === 2
+          ? '<meta property="fc:frame:button:1" content="Yes" />'
+          : ''
+      }
+    </head>
+    <body>      
+    </body>
+    </html>
+  `
+}
+
+async function getWord(type){
+  const apiUrl = 'https://api.api-ninjas.com/v1/randomword?type='+type; // Replace with your API endpoint
+  const apiKey = '7+3g7iiysipWcwzyopjvKA==vmtMx1li43hUsZBO'; // Replace with your actual API key
+  console.log(apiUrl)
+  const axiosConfig = {
+    headers: {
+      'X-Api-Key': apiKey,
+    },
+  };
+
+  let word = axios.get(apiUrl, axiosConfig)
+  .then((response) => {
+    const responseData = response.data;
+    return responseData.word
+  })
+  .catch((error) => {
+    console.error('Error:', error);
+  });
+  return word
 }
