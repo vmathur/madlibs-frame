c0ca2cf0644e477d312d8f12d1922bf997d10117
vmathur <vmathur@users.noreply.github.com>
2024-01-31 22:51:14 -0800

    css styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react'
 
 export default function Home() {
   let BASE_URL = process.env.NEXT_PUBLIC_BASE_URL
+  let gameUrl = 'https://google.com'
   const [data, setData] = useState(null)
   const [adjective, setAdjective] = useState([])
   const [noun, setNoun] = useState([])
@@ -26,10 +27,9 @@ export default function Home() {
     <div className='main'>
       <h1>Madcaster</h1>
       <h2>The Adventure of the {getValue(adjective[0])} {getValue(noun[0])}</h2>
-      <div>Once upon a time in a {getValue(adjective[1])} {getValue(noun[1])}, there lived a {getValue(adjective[0])} {getValue(noun[0])}. This {getValue(noun[0])} was known throughout the land for their incredible {getValue(adjective[2])} {getValue(noun[2])}. One day, they decided to embark on a(n) {getValue(adjective[3])} adventure.</div>
-      {/* <div>They packed their [Noun] with all the necessary [Plural Noun] and set off on their journey. Along the way, they encountered a [Adjective] [Animal] who offered to join them on their quest. Together, they faced many [Adjective] challenges and overcame them with their [Adjective] [Plural Noun].</div>
-      <div>Finally, after a long and [Adjective] journey, they reached the top of a [Adjective] [Mountain]. At the summit, they found a [Noun] made of [Precious Gem]. It was said that anyone who possessed this [Noun] would be granted [Adjective] [Superpower].</div>
-      <div>With their newfound [Noun], our hero returned to their [Noun], where they used their [Superpower] to bring [Adjective] [Noun] to their kingdom. And so, they lived [Adverb] ever after.</div> */}
+      <p className='section-1'>Once upon a time in a {getValue(adjective[1])} {getValue(noun[1])}, there lived a {getValue(adjective[0])} {getValue(noun[0])}. This {getValue(noun[0])} was known throughout the land for their incredible {getValue(adjective[2])} {getValue(noun[2])}. One day, they decided to embark on a(n) {getValue(adjective[3])} adventure.</p>
+      <p className='section-2'>They packed their {getValue(noun[3])} with all the necessary {getValue(noun[4])} and set off on their journey. Along the way, they encountered a {getValue(adjective[4])} horse who offered to join them on their quest. Together, they faced many {getValue(adjective[5])} challenges and overcame them with their {getValue(adjective[6])} {getValue(noun[2])}.</p>
+      <div className='footer'>Play <a href={gameUrl}>here</a> on warpcaster</div>
     </div>
   );
 }
